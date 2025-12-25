f52b8b20eb0017ea8a71773e33792fce01c1132e
Diode-exe <DiodesCAER@protonmail.com>
2025-12-25 00:12:21 -0600

    fixed timestamp bug

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ class deadDirScanCls():
                 try:
                     mtime = os.path.getmtime(path)
                     human_time = datetime.fromtimestamp(mtime).astimezone()
+                    human_time = human_time.replace(microsecond=0)
+                    formatted_human_time = human_time.strftime("%Y-%m-%d %H:%M:%S %z")
                     data.append((path, mtime, human_time.isoformat()))
                     # print(data)
                 except (PermissionError, FileNotFoundError, OSError):
                     continue
         data.sort(key=lambda item: item[1])
         print(data)
-        with open("deadDir.txt", "w") as f:
-            for (path, mtime, human_time) in data:
-                f.write(f"{path} {mtime} {human_time}\n")
-        
+        with open("deadDir.txt", "w", encoding="utf-8") as f:
+            for path, mtime, human_time_iso in data:
+                f.write(f"{path} {mtime} {human_time_iso}\n")
+
 root = r"C:\\Users\\Rohan\\Documents\\python"
 deadDirScanCls.scan(root)
