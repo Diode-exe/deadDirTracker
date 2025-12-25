fb78c3c4fa9823efd9b578d1b9a331ed06a02163
Diode-exe <DiodesCAER@protonmail.com>
2025-12-24 23:50:52 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from datetime import datetime
+
+class deadDirScanCls():
+    def scan(root):
+        data = []
+        for dirpath, dirnames, filenames in os.walk(root):
+            for name in filenames:
+                path = os.path.join(dirpath, name)
+                try:
+                    mtime = os.path.getmtime(path)
+                    human_time = datetime.fromtimestamp(mtime).astimezone()
+                    data.append((path, mtime, human_time.isoformat()))
+                    # print(data)
+                except (PermissionError, FileNotFoundError):
+                    continue
+        data.sort(key=lambda item: item[1])
+        print(data)
+        
+root = r"C:\\Users\\Rohan\\Documents\\python"
+deadDirScanCls.scan(root)
